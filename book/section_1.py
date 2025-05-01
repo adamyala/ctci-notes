3e031726012a52e5d6f56071d43a23835eb73447
adamyala <adamkyala@gmail.com>
2025-05-01 10:17:46 -0600

    add 1.3 problem

--- book/section_1.py
+++ book/section_1.py
@@ -18,3 +18,22 @@ def unique_letters_no_structs(input_string):
 def check_permutation(one, two):
     from itertools import permutations
     return tuple(two) in list(permutations(one))
+
+
+def check_permutation_no_libs(one, two):
+    set_one = set(one)
+    set_two = set(two)
+    missing_from_two = set_one.difference(set_two)
+    missing_from_one = set_two.difference(set_one)
+    if not missing_from_one and not missing_from_two:
+        return True
+
+
+# 1.3
+# URLify: Write a method to replace all spaces in a string with '%20'.
+# You may assume that the string has sufficient space at the end to hold the additional
+# characters,and that you are given the "true" length of the string.
+def urlify(input_string, some_int):
+    words = input_string.split()
+    urlified = '%20'.join(words)
+    return urlified
--- tests/test_book/test_1.py
+++ tests/test_book/test_1.py
@@ -5,11 +5,16 @@ from book import section_1
 
 class TestSection1(unittest.TestCase):
     def test_section_1_1(self):
-        assert section_1.unique_letters('abcdef')
-        assert not section_1.unique_letters('abcdefa')
-        assert section_1.unique_letters_no_structs('abcdef')
-        assert not section_1.unique_letters_no_structs('abcdefa')
+        assert section_1.unique_letters(input_string='abcdef')
+        assert not section_1.unique_letters(input_string='abcdefa')
+        assert section_1.unique_letters_no_structs(input_string='abcdef')
+        assert not section_1.unique_letters_no_structs(input_string='abcdefa')
 
     def test_section_1_2(self):
-        assert section_1.check_permutation('abc', 'bca')
-        assert not section_1.check_permutation('abc', 'bcd')
+        assert section_1.check_permutation(one='abc', two='bca')
+        assert not section_1.check_permutation(one='abc', two='bcd')
+        assert section_1.check_permutation_no_libs(one='abc', two='bca')
+        assert not section_1.check_permutation_no_libs(one='abc', two='bcd')
+
+    def test_section_1_3(self):
+        assert section_1.urlify(input_string='Mr  John   Smith    ', some_int=13) == 'Mr%20John%20Smith'
